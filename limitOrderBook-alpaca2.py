159edb1622362246e74f8788dc71a9b6264ec2f4
Asmaa Abdul-Amin <Asmaa.Abdul-Amin@jhuapl.edu>
2023-06-27 14:24:47 -0400

    Updated code to execute sell orders only if there is a position open

--- limitOrderBook-alpaca2.py
+++ limitOrderBook-alpaca2.py
@@ -14,106 +14,139 @@ BASE_URL = 'https://paper-api.alpaca.markets'  # Use the paper trading API URL f
 # Initialize the Alpaca API
 api = tradeapi.REST(API_KEY, SECRET_KEY, base_url=BASE_URL, api_version='v2')
 
-# Strategy parameters
-ma_1 = 5
-ma_2 = 20
+while True:
+    # Strategy parameters
+    ma_1 = 5
+    ma_2 = 20
 
-# Get the current date and time
-now = dt.datetime.now()
+    # Get the current date and time
+    now = dt.datetime.now()
 
-# Set the start and end dates for data retrieval
-start = now - dt.timedelta(days=365 * 4)
-end = now
+    # Set the start and end dates for data retrieval
+    start = now - dt.timedelta(days=365 * 4)
+    end = now
 
-# Download historical data from Yahoo Finance
-data = yf.download('JEPQ', start=start, end=end)
+    # Download historical data from Yahoo Finance
+    data = yf.download('JEPQ', start=start, end=end)
 
-# Calculate moving averages
-data[f'SMA_{ma_1}'] = data['Adj Close'].rolling(window=ma_1).mean()
-data[f'SMA_{ma_2}'] = data['Adj Close'].rolling(window=ma_2).mean()
+    # Calculate moving averages
+    data[f'SMA_{ma_1}'] = data['Adj Close'].rolling(window=ma_1).mean()
+    data[f'SMA_{ma_2}'] = data['Adj Close'].rolling(window=ma_2).mean()
 
-# Generate buy and sell signals
-data = data.iloc[ma_2:]
+    # Rest of the code...
 
-buy_signals = [np.nan]  # Add initial nan value
-sell_signals = [np.nan]  # Add initial nan value
-trigger = 0
-position = 0  # Current position (0: flat, 1: long, -1: short)
-order_ids = []  # List to track order IDs
+    # Generate buy and sell signals
+    data = data.iloc[ma_2:]
 
-for x in range(1, len(data)):
-    if (
-        data[f'SMA_{ma_1}'].iloc[x] > data[f'SMA_{ma_2}'].iloc[x] and
-        data[f'SMA_{ma_1}'].iloc[x - 1] < data[f'SMA_{ma_2}'].iloc[x - 1] and
-        position != 1
-    ):
-        buy_signals.append(data['Adj Close'].iloc[x])
-        sell_signals.append(np.nan)
-        position = 1
-        # Place a buy order using Alpaca API
-        order = api.submit_order(
-            symbol='SOXL',
-            qty=1,  # Number of shares to buy
-            side='buy',
-            type='market',
-            time_in_force='gtc'
-        )
-        order_ids.append(order.id)
-        # Set position to 0 to prevent short positions
-        position = 0
-    else:
-        buy_signals.append(np.nan)
-        sell_signals.append(np.nan)
+    buy_signals = [np.nan]  # Add initial nan value
+    sell_signals = [np.nan]  # Add initial nan value
+    trigger = 0
+    position = 0  # Current position (0: flat, 1: long, -1: short)
+    order_ids = []  # List to track order IDs
 
-# Calculate strategy returns
-data['Position'] = np.where(data['Buy Signals'].notnull(), 1, np.where(data['Sell Signals'].notnull(), 0, np.nan))
-data['Position'] = data['Position'].ffill().fillna(0)
-data['Market Return'] = data['Adj Close'].pct_change()
-data['Strategy Return'] = data['Market Return'] * data['Position']
-data['Cumulative Returns'] = (data['Strategy Return'] + 1).cumprod() * 1000  # Starting portfolio value: $1000
-ending_portfolio_value = data['Cumulative Returns'].iloc[-1]
+    for x in range(1, len(data)):
+        if (
+            data[f'SMA_{ma_1}'].iloc[x] > data[f'SMA_{ma_2}'].iloc[x] and
+            data[f'SMA_{ma_1}'].iloc[x - 1] < data[f'SMA_{ma_2}'].iloc[x - 1] and
+            position != 1
+        ):
+            buy_signals.append(data['Adj Close'].iloc[x])
+            sell_signals.append(np.nan)
+            position = 1
+            # Place a buy order using Alpaca API
+            order = api.submit_order(
+                symbol='SOXL',
+                qty=1,  # Number of shares to buy
+                side='buy',
+                type='market',
+                time_in_force='gtc'
+            )
+            order_ids.append(order.id)
+            # Set position to 0 to prevent short positions
+            position = 0
+        elif (
+            data[f'SMA_{ma_1}'].iloc[x] < data[f'SMA_{ma_2}'].iloc[x] and
+            data[f'SMA_{ma_1}'].iloc[x - 1] > data[f'SMA_{ma_2}'].iloc[x - 1] and
+            position == 1
+        ):
+            buy_signals.append(np.nan)
+            sell_signals.append(data['Adj Close'].iloc[x])
+            position = 0
+            # Place a sell order using Alpaca API
+            order = api.submit_order(
+                symbol='SOXL',
+                qty=1,  # Number of shares to sell
+                side='sell',
+                type='market',
+                time_in_force='gtc'
+            )
+            order_ids.append(order.id)
+        else:
+            buy_signals.append(np.nan)
+            sell_signals.append(np.nan)
 
-# Calculate alpha, beta, and Sharpe ratio
-risk_free_rate = 0  # Assuming risk-free rate as 0%
+    # Calculate strategy returns and other metrics
+    data['Buy Signals'] = buy_signals
+    data['Sell Signals'] = sell_signals
+    data['Position'] = np.where(data['Buy Signals'].notnull(), 1, np.where(data['Sell Signals'].notnull(), 0, np.nan))
+    data['Position'] = data['Position'].ffill().fillna(0)
+    data['Market Return'] = data['Adj Close'].pct_change()
+    data['Strategy Return'] = data['Market Return'] * data['Position']
+    data['Cumulative Returns'] = (data['Strategy Return'] + 1).cumprod() * 1000  # Starting portfolio value: $1000
+    ending_portfolio_value = data['Cumulative Returns'].iloc[-1]
 
-benchmark_returns = data['Market Return'].dropna()
-strategy_returns = data['Strategy Return'].dropna()
+    # Calculate alpha, beta, and Sharpe ratio
+    risk_free_rate = 0  # Assuming risk-free rate as 0%
 
-benchmark_excess_returns = benchmark_returns - risk_free_rate
-strategy_excess_returns = strategy_returns - risk_free_rate
+    benchmark_returns = data['Market Return'].dropna()
+    strategy_returns = data['Strategy Return'].dropna()
 
-beta, alpha = np.polyfit(benchmark_excess_returns, strategy_excess_returns, deg=1)
-cumulative_returns = data['Cumulative Returns'].iloc[-1]
-sharpe_ratio = np.sqrt(252) * np.mean(strategy_returns) / np.std(strategy_returns)
+    benchmark_excess_returns = benchmark_returns - risk_free_rate
+    strategy_excess_returns = strategy_returns - risk_free_rate
 
-print("Alpha:", alpha)
-print("Beta:", beta)
-print("Cumulative Returns:", cumulative_returns)
-print("Sharpe Ratio:", sharpe_ratio)
-print("Ending Portfolio Value:", ending_portfolio_value)
+    beta, alpha = np.polyfit(benchmark_excess_returns, strategy_excess_returns, deg=1)
+    cumulative_returns = data['Cumulative Returns'].iloc[-1]
+    sharpe_ratio = np.sqrt(252) * np.mean(strategy_returns) / np.std(strategy_returns)
 
-plt.plot(data['Adj Close'], label="Share Price", alpha=0.5)
-plt.plot(data[f'SMA_{ma_1}'], label=f"SMA_{ma_1}", color="aqua", linestyle="--")
-plt.plot(data[f'SMA_{ma_2}'], label=f"SMA_{ma_2}", color="lime", linestyle="--")
-plt.scatter(data.index, data['Buy Signals'], label="Buy Signal", marker="^", color="#00ff00", linewidths=3)
-plt.scatter(data.index, data['Sell Signals'], label="Sell Signal", marker="v", color="#ff0000", linewidths=3)
-plt.legend(loc="upper left")
-plt.show()
+    print("Alpha:", alpha)
+    print("Beta:", beta)
+    print("Cumulative Returns:", cumulative_returns)
+    print("Sharpe Ratio:", sharpe_ratio)
+    print("Ending Portfolio Value:", ending_portfolio_value)
 
-current_price = data['Adj Close'].iloc[-1]  # Assuming current price is the last price in the data
+    plt.plot(data['Adj Close'], label="Share Price", alpha=0.5)
+    plt.plot(data[f'SMA_{ma_1}'], label=f"SMA_{ma_1}", color="aqua", linestyle="--")
+    plt.plot(data[f'SMA_{ma_2}'], label=f"SMA_{ma_2}", color="lime", linestyle="--")
+    plt.scatter(data.index, data['Buy Signals'], label="Buy Signal", marker="^", color="#00ff00", linewidths=3)
+    plt.scatter(data.index, data['Sell Signals'], label="Sell Signal", marker="v", color="#ff0000", linewidths=3)
+    plt.legend(loc="upper left")
+    plt.show()
 
-# Execute buy orders and print executed orders
-executed_buy_orders = []
-for order_id in order_ids:
-    order = api.get_order_by_client_order_id(order_id)
-    if order.side == 'buy' and order.status == 'filled':
-        executed_buy_orders.append(order)
-        print("Executed Buy Order:")
-        print("Symbol:", order.symbol)
-        print("Price:", order.filled_avg_price)
-        print("Quantity:", order.filled_qty)
-        print("Order ID:", order.id)
-        print()
+    # Execute buy orders and print executed orders
+    executed_buy_orders = []
+    for order_id in order_ids:
+        order = api.get_order_by_client_order_id(order_id)
+        if order.side == 'buy' and order.status == 'filled':
+            executed_buy_orders.append(order)
+            print("Executed Buy Order:")
+            print("Symbol:", order.symbol)
+            print("Price:", order.filled_avg_price)
+            print("Quantity:", order.filled_qty)
+            print("Order ID:", order.id)
+            print()
 
-# Clear order_ids list
-order_ids = []
+    # Execute sell orders and print executed orders
+    executed_sell_orders = []
+    for order_id in order_ids:
+        order = api.get_order_by_client_order_id(order_id)
+        if order.side == 'sell' and order.status == 'filled':
+            executed_sell_orders.append(order)
+            print("Executed Sell Order:")
+            print("Symbol:", order.symbol)
+            print("Price:", order.filled_avg_price)
+            print("Quantity:", order.filled_qty)
+            print("Order ID:", order.id)
+            print()
+
+    # Clear order_ids list
+    order_ids = []
